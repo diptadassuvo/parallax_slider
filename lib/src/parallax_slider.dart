8bb747010f93feafe92088c35ac4a6fe5f106da6
Dipta Das <diptadassuvo@gmail.com>
2024-07-05 11:44:09 +0600

    init project

---
+++ lib/src/parallax_slider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ParallaxSlider extends StatefulWidget {
+  const ParallaxSlider({super.key});
+
+  @override
+  State<ParallaxSlider> createState() => _ParallaxSliderState();
+}
+
+class _ParallaxSliderState extends State<ParallaxSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ test/src/parallax_slider_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:parallax_slider/parallax_slider.dart';
+
+void main() {
+  group('ParallaxSlider', () {
+    test('can be instantiated', () {
+      expect(ParallaxSlider(), isNotNull);
+    });
+  });
+}
